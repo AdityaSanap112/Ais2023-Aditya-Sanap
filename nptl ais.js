ce6d5427a18a1bddab1c3bae5199d87b74b358c7
Aditya Sanap <55378862+AdityaSanap112@users.noreply.github.com>
2023-03-20 18:58:11 +0530

    Add files via upload

---
+++ nptl ais.js
@@ -0,0 +1,12 @@
+function fun(){
+var username=document.getElementById("username").value
+var password=document.getElementById("password").value
+    if(password=="")
+    {
+        alert("password can not be empty")
+    }
+    if(username=="")
+    {
+        alert("username can not be empty")
+    }
+}
